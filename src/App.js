5f85c37e593f1f8e3e24f2c06be5fe8b74e0e826
Beringer Dorottya <beringerdorottya@gmail.com>
2023-12-17 21:44:21 +0100

    d3 setup, experiment with react and d3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import * as d3 from 'd3';
+
+class BarChart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.drawChart();
+    this.loadData();
+  }
+
+  loadData() {
+    document.addEventListener('DOMContentLoaded', function () {
+      fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+        .then(response => response.json())
+        .then(data => {
+          const GDPdata = Object.values(data.data);
+          console.log(GDPdata);
+        });
+    });
+  }
+
+  drawChart() {
+    const svg = d3.select('body').append('svg').attr('width', 200).attr('height', 100).style('background-color', 'red');
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
 
 function App() {
+  <script>
+    document.addEventListener('DOMContentLoaded', function()
+    {fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+      .then(response => response.json())
+      .then(data => {
+        const GDPdata = Object.values(data.data);
+      })}
+    ); const svg = d3.select('body').append('svg').attr('width', 200).attr('height', 100).style('background-color', 'red');
+  </script>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">United States GDP</h1>
+      <BarChart />
     </div>
   );
 }
