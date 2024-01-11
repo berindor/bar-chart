ff38e46639cb9d501e18e06de3070c8d5ae7fcb8
Beringer Dorottya <beringerdorottya@gmail.com>
2024-01-11 18:28:48 +0100

    added legend and formatting all tests passed

--- src/ScatterPlot.js
+++ src/ScatterPlot.js
@@ -1,7 +1,6 @@
 import './ScatterPlot.css';
 import React from 'react';
 import * as d3 from 'd3';
-import { utcFormat } from 'd3';
 
 class ScatterPlot extends React.Component {
   async componentDidMount() {
@@ -26,25 +25,25 @@ class ScatterPlot extends React.Component {
     const n = dataset.length;
     const minTime = d3.min(dataset, d => d.TimeInDate);
     const maxTime = d3.max(dataset, d => d.TimeInDate);
+    const minYaxis = new Date(d3.min(dataset, d => d.Seconds) * 1000 - 10 * 1000);
+    const maxYaxis = new Date(d3.max(dataset, d => d.Seconds) * 1000 + 10 * 1000);
     const minYear = d3.min(dataset, d => d.Year);
     const maxYear = d3.max(dataset, d => d.Year);
     console.log('number of data: ' + n + ', min time: ' + minTime + ' sec, max: ' + maxTime + ' sec');
-    console.log(minTime.toLocaleString(undefined, { minute: 'numeric', second: 'numeric' }));
     console.log('Years: ' + minYear + ' to ' + maxYear);
     const w = 900;
     const h = 800;
     const padding = 50;
-    d3.select('svg').remove();
+    d3.selectAll('svg').remove();
     d3.select('.tooltip').remove();
-    d3.select('.legend').remove();
     const xScale = d3
       .scaleLinear()
       .domain([minYear - 0.5, maxYear + 0.5])
-      .range([padding, w - padding]);
+      .range([1.5 * padding, w - padding]);
     const xAxis = d3.axisBottom(xScale).tickFormat(d3.format(''));
     const yScale = d3
       .scaleTime()
-      .domain([minTime, maxTime])
+      .domain([maxYaxis, minYaxis])
       .range([h - padding, padding]);
     const yAxis = d3.axisLeft(yScale).tickFormat(d3.utcFormat('%M:%S'));
     const svg = d3.selectAll('.App').append('svg').attr('width', w).attr('height', h).style('background-color', 'green');
@@ -52,11 +51,10 @@ class ScatterPlot extends React.Component {
     const mouseover = function (event) {
       const year = event.target.getAttribute('data-xvalue');
       const time = event.target.getAttribute('data-time');
-      const timeInDate = event.target.getAttribute('data-yvalue');
       const name = event.target.getAttribute('data-name');
       const nationality = event.target.getAttribute('data-nationality');
       const doping = event.target.getAttribute('data-doping');
-      console.log(event.target);
+      const htmlText = name + ', ' + nationality + '<br> year: ' + year + ', time: ' + time + (doping === '' ? '' : '<br> Doping: ' + doping);
       tooltip
         .style('visibility', 'visible')
         .attr('data-year', year)
@@ -64,9 +62,9 @@ class ScatterPlot extends React.Component {
         .attr('data-name', name)
         .attr('data-nationality', nationality)
         .attr('data-doping', doping)
-        .html(name + ', ' + nationality + '<br> year: ' + year + ', time: ' + time + '<br> Doping: ' + doping)
+        .html(htmlText)
         .style('left', event.target.getAttribute('cx') - w / 2 + 'px')
-        .style('top', event.target.getAttribute('cy') - padding - h - 25 + 'px')
+        .style('top', event.target.getAttribute('cy') - padding - h - (doping === '' ? 5 : 25) + 'px')
         .style('background-color', doping === '' ? 'white' : 'orange');
     };
     const mouseout = function () {
@@ -100,8 +98,50 @@ class ScatterPlot extends React.Component {
     svg
       .append('g')
       .attr('id', 'y-axis')
-      .attr('transform', 'translate(' + padding + ', 0)')
+      .attr('transform', 'translate(' + 1.5 * padding + ', 0)')
       .call(yAxis);
+    svg
+      .append('text')
+      .attr('text-anchor', 'end')
+      .attr('transform', 'rotate(-90)')
+      .attr('x', -h / 2 + padding)
+      .attr('y', padding / 2)
+      .text('time in minutes');
+
+    d3.selectAll('#legend').remove();
+    d3.selectAll('.App').append('div').attr('id', 'legend').style('backgroound-color', 'black');
+    this.drawLegend();
+  }
+
+  drawLegend() {
+    const lh = 40;
+    const lw = 40;
+    const colors = ['white', 'orange'];
+    const legendSelect = d3.selectAll('#legend');
+    legendSelect.selectAll('svg').remove();
+    legendSelect.selectAll('div').remove();
+    legendSelect
+      .selectAll('svg')
+      .data(colors)
+      .enter()
+      .append('svg')
+      .attr('width', lw)
+      .attr('height', lh)
+      .style('background-color', 'green')
+      .append('circle')
+      .attr('cx', lw / 2)
+      .attr('cy', lh / 2)
+      .attr('r', 8)
+      .attr('fill', d => d);
+    const legendText = ['no doping', 'doping'];
+    legendSelect
+      .selectAll('div')
+      .data(legendText)
+      .enter()
+      .append('div')
+      .attr('height', lh)
+      .attr('class', 'legend-text-div')
+      .html(d => d);
   }
 
   render() {
