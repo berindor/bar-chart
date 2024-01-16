4b77455a060e2837de774ba2fec3cad7d067a9d6
Beringer Dorottya <beringerdorottya@gmail.com>
2024-01-16 13:22:55 +0100

    added state to HeatMap

--- src/HeatMap.js
+++ src/HeatMap.js
@@ -3,41 +3,72 @@ import React from 'react';
 import * as d3 from 'd3';
 
 class HeatMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      baseTemperature: undefined,
+      monthlyVariance: [],
+      minVariance: undefined,
+      maxVariance: undefined
+    };
+    this.drawPage = this.drawPage.bind(this);
+    this.setColorBorders = this.setColorBorders.bind(this);
+    this.setColor = this.setColor.bind(this);
+  }
+
   async componentDidMount() {
     const dataset = await this.loadData();
-    this.createSubtitle(dataset);
-    this.drawChart(dataset);
-    this.drawLegend(dataset);
+    this.setState(
+      {
+        baseTemperature: dataset.baseTemperature,
+        monthlyVariance: dataset.monthlyVariance,
+        minVariance: d3.min(dataset.monthlyVariance, d => d.variance),
+        maxVariance: d3.max(dataset.monthlyVariance, d => d.variance)
+      },
+      this.drawPage
+    );
+  }
+
+  drawPage() {
+    console.log(this.state);
+    this.createSubtitle();
+    this.drawChart();
+    this.drawLegend();
   }
 
   async loadData() {
     return await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         return data;
       });
   }
 
-  createSubtitle(dataset) {
-    const baseTemperature = dataset.baseTemperature;
-    const yearMin = dataset.monthlyVariance[0].year;
-    const dataLength = dataset.monthlyVariance.length;
-    const yearMax = dataset.monthlyVariance[dataLength - 1].year;
+  setColorBorders(numberOfColors) {
+    let arr = [];
+    for (let i = 0; i < numberOfColors + 1; i++) {
+      arr.push(this.state.baseTemperature + this.state.minVariance + ((this.state.maxVariance - this.state.minVariance) * i) / numberOfColors);
+    }
+    return arr;
+  }
+
+  setColor(temperature) {}
+
+  createSubtitle() {
+    const yearMin = this.state.monthlyVariance[0].year;
+    const dataLength = this.state.monthlyVariance.length;
+    const yearMax = this.state.monthlyVariance[dataLength - 1].year;
     d3.selectAll('h2').remove();
     d3.selectAll('#heat-map')
       .append('h2')
       .attr('id', 'description')
-      .text(yearMin + ' - ' + yearMax + ' base temperature: ' + baseTemperature + '°C');
+      .text(yearMin + ' - ' + yearMax + ' base temperature: ' + this.state.baseTemperature + '°C');
   }
 
-  drawChart(dataset) {
-    const baseTemperature = dataset.baseTemperature;
-    const monthlyVariance = dataset.monthlyVariance;
-    const yearMin = dataset.monthlyVariance[0].year;
-    const dataLength = dataset.monthlyVariance.length;
-    const yearMax = dataset.monthlyVariance[dataLength - 1].year;
-    const numberOfYears = yearMax - yearMin + 1;
+  drawChart() {
+    const yearMin = this.state.monthlyVariance[0].year;
+    const dataLength = this.state.monthlyVariance.length;
+    const yearMax = this.state.monthlyVariance[dataLength - 1].year;
     const padding = 30;
     const height = 500;
     const width = 1000;
@@ -74,11 +105,11 @@ class HeatMap extends React.Component {
       .call(yAxis);
   }
 
-  drawLegend(dataset) {
-    const baseTemperature = dataset.baseTemperature;
-    const monthlyVariance = dataset.monthlyVariance;
-    const minVariance = d3.min(monthlyVariance, d => d.variance);
-    const maxVariance = d3.max(monthlyVariance, d => d.variance);
+  drawLegend() {
+    const baseTemperature = this.state.baseTemperature;
+    const monthlyVariance = this.state.monthlyVariance;
+    const minVariance = this.state.minVariance;
+    const maxVariance = this.state.maxVariance;
     const legendHeight = 100;
     const legendWidth = 400;
     const legendPadding = 20;
