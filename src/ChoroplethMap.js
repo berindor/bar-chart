28a01929a6e3080d7add2488940853cf8edf491d
Beringer Dorottya <beringerdorottya@gmail.com>
2024-01-17 16:51:13 +0100

    experiment with geoJson

--- src/ChoroplethMap.js
+++ src/ChoroplethMap.js
@@ -29,14 +29,99 @@ class ChoroplethMap extends React.Component {
     d3.select('#map').remove();
     const svgMap = chartDiv.append('svg').attr('id', 'map').attr('width', mapWidth).attr('height', mapHeight).style('background-color', 'green');
 
-    const projection = d3
-      .geoMercator()
-      .scale([180])
-      .translate([mapWidth / 3, mapHeight / 3]);
+    const arcsCoord = countyData.arcs;
+    const nationObj = countyData.objects.nation;
+    console.log('nationObj with arc id-s: ', nationObj);
 
-    const geoGenerator = d3.geoPath(projection);
+    const arcIdToCoord = id => {
+      return id > 0 ? arcsCoord[id] : [arcsCoord[-1 * id][1], arcsCoord[-1 * id][0]];
+    };
 
-    svgMap.append('g').selectAll('path').data(countyData).enter().append('path').attr('d', geoGenerator).attr('fill', 'orange');
+    const setArcsCoord = arr => {
+      let newArr = [];
+      for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number') {
+          newArr.push(arcIdToCoord(arr[i]));
+        } else {
+          newArr.push(setArcsCoord(arr[i]));
+        }
+      }
+      return newArr;
+    };
+
+    const nationArcsCoord = nationObj.geometries[0].arcs.map(arr => setArcsCoord(arr));
+    var nationCoord = { ...nationObj };
+    nationCoord.geometries[0].arcs = nationArcsCoord;
+    nationCoord.geometries[0].coordinates = nationArcsCoord;
+    console.log('nationObj with arc coordinates', nationCoord);
+    const geojsonNation = [{ type: 'Feature', properties: { name: 'USA' }, geometry: nationCoord.geometries[0] }]; //ezzel se működik a kirajzolás
+
+    const geojson = {
+      //nem kell, csak próba
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {
+            name: 'Africa'
+          },
+          geometry: {
+            type: 'Polygon',
+            coordinates: [
+              [
+                [-6, 36],
+                [33, 30],
+                [43, 11],
+                [51, 12],
+                [29, -33],
+                [18, -35],
+                [7, 5],
+                [-17, 14],
+                [-6, 36]
+              ]
+            ]
+          }
+        },
+        {
+          type: 'Feature',
+          properties: {
+            name: 'Australia'
+          },
+          geometry: {
+            type: 'Polygon',
+            coordinates: [
+              [
+                [143, -11],
+                [153, -28],
+                [144, -38],
+                [131, -31],
+                [116, -35],
+                [114, -22],
+                [136, -12],
+                [140, -17],
+                [143, -11]
+              ]
+            ]
+          }
+        },
+        {
+          type: 'Feature',
+          properties: {
+            name: 'Timbuktu'
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [-3.0026, 16.7666]
+          }
+        }
+      ]
+    };
+
+    const projection = d3.geoEquirectangular().translate([mapWidth / 4, mapHeight / 3]);
+
+    const geoGenerator = d3.geoPath().projection(projection);
+
+    svgMap.append('g').selectAll('path').data(geojson.features).enter().append('path').attr('d', geoGenerator).attr('fill', 'orange');
   }
 
   render() {
