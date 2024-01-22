9e1c299089b829751a879cfb46ebfab68e5712e0
Beringer Dorottya <beringerdorottya@gmail.com>
2024-01-22 12:58:37 +0100

    added data to counties in ChoropletMap

--- src/ChoroplethMap.js
+++ src/ChoroplethMap.js
@@ -2,8 +2,6 @@ import './ChoroplethMap.scss';
 import React from 'react';
 import * as topojson from 'topojson';
 import * as d3 from 'd3';
-import * as turf from '@turf/turf';
-import { geojsonType } from '@turf/turf';
 
 class ChoroplethMap extends React.Component {
   async componentDidMount() {
@@ -23,92 +21,46 @@ class ChoroplethMap extends React.Component {
   }
 
   drawChart(educationData, countyData) {
-    //    console.log(educationData[0]);
-    console.log(countyData);
+    console.log('educationData[0]: ', educationData[0]);
+    console.log('countyData: ', countyData);
+    const mapWidth = 1000;
+    const mapHeight = 600;
     d3.select('#map-div').remove();
-    const mapWidth = 700;
-    const mapHeight = 500;
     const chartDiv = d3.selectAll('#choropleth-map').append('div').attr('id', 'map-div');
     d3.select('#map').remove();
     const svgMap = chartDiv.append('svg').attr('id', 'map').attr('width', mapWidth).attr('height', mapHeight).style('background-color', 'green');
 
     const geojsonNation = topojson.feature(countyData, countyData.objects.nation);
-    console.log('topojson to geojson: ', geojsonNation);
+    const geojsonStates = topojson.feature(countyData, countyData.objects.states);
+    const geojsonCounties = topojson.feature(countyData, countyData.objects.counties);
 
-    const geojson = {
-      //nem kell, csak próba
-      type: 'FeatureCollection',
-      features: [
-        {
-          type: 'Feature',
-          properties: {
-            name: 'Africa'
-          },
-          geometry: {
-            type: 'Polygon',
-            coordinates: [
-              [
-                [-6, 36],
-                [33, 30],
-                [43, 11],
-                [51, 12],
-                [29, -33],
-                [18, -35],
-                [7, 5],
-                [-17, 14],
-                [-6, 36]
-              ]
-            ]
-          }
-        },
-        {
-          type: 'Feature',
-          properties: {
-            name: 'Australia'
-          },
-          geometry: {
-            type: 'Polygon',
-            coordinates: [
-              [
-                [143, -11],
-                [153, -28],
-                [144, -38],
-                [131, -31],
-                [116, -35],
-                [114, -22],
-                [136, -12],
-                [140, -17],
-                [143, -11]
-              ]
-            ]
-          }
-        },
-        {
-          type: 'Feature',
-          properties: {
-            name: 'Timbuktu'
-          },
-          geometry: {
-            type: 'Point',
-            coordinates: [-3.0026, 16.7666]
-          }
-        }
-      ]
-    };
+    const geoGenerator = d3.geoPath();
 
-    const projection = d3.geoEquirectangular(); //nem kell
-    projection.fitExtent(
-      //nem kell
-      [
-        [0, 0],
-        [mapWidth, mapHeight]
-      ],
-      geojsonNation
-    );
+    const gMap = svgMap.append('g').attr('id', 'gMap');
 
-    const geoGenerator = d3.geoPath().projection(null);
-
-    svgMap.append('g').selectAll('path').data(geojsonNation.features).enter().append('path').attr('d', geoGenerator);
+    const nationPath = gMap.selectAll('.nation').data(geojsonNation.features).enter().append('path').attr('d', geoGenerator).attr('class', 'nation');
+    nationPath.attr('fill-opacity', '0').attr('stroke', 'red');
+    const statesPath = gMap.selectAll('.states').data(geojsonStates.features).enter().append('path').attr('d', geoGenerator).attr('class', 'states');
+    statesPath.attr('fill', 'orange');
+    const countiesPath = gMap
+      .selectAll('.county')
+      .data(geojsonCounties.features)
+      .enter()
+      .append('path')
+      .attr('d', geoGenerator)
+      .attr('class', 'county')
+      .attr('data-fips', d => d.id)
+      .attr('data-education', d => {
+        let obj = educationData.find(obj => obj.fips === d.id);
+        return obj.bachelorsOrHigher;
+      });
+    countiesPath.attr('fill-opacity', '0');
+    const minId = d3.min(geojsonCounties.features, d => d.id);
+    const maxId = d3.max(geojsonCounties.features, d => d.id);
+    console.log('min id: ', minId, 'max id: ', maxId, 'in geojsonCounties.features with length ', geojsonCounties.features.length);
+    const minFips = d3.min(educationData, d => d.fips);
+    const maxFips = d3.max(educationData, d => d.fips);
+    console.log('min fips: ', minFips, 'max fips: ', maxFips, 'in educationData with length ', educationData.length);
   }
 
   render() {
@@ -120,6 +72,7 @@ function ChoroplethMapPage() {
   return (
     <div className="choropleth-map-page">
       <h1 id="title">United States Educational Attainment</h1>
+      <div id="description">Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)</div>
       <ChoroplethMap />
     </div>
   );
